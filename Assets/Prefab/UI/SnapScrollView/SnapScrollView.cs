a4e85b3bbf39e7ee1cae179e603872b4d4736fb5
IwazakiMakoto <nekozita1029@gmail.com>
2019-05-05 18:45:03 +0900

    チュートリアルウインドウと一時停止機能を実装(改良途中)

---
+++ Assets/Prefab/UI/SnapScrollView/SnapScrollView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SnapScrollView
+{
+    public class SnapScrollView : MonoBehaviour
+    {
+
+        [SerializeField] SnapScrollRect scrollView;
+        [SerializeField] Image[] indicators;
+
+        void Start()
+        {
+            Application.targetFrameRate = 30;
+
+            scrollView.OnPageChanged += OnIndicatorUpdate;
+            scrollView.RefreshPage();
+        }
+        
+        void OnIndicatorUpdate()
+        {
+            for(var i = 0; i < indicators.Length; i++)
+            {
+                var a = (i == scrollView.Page) ? 1 : 0.5f;
+                indicators[i].color = new Color(1, 1, 1, a);
+            }
+        }
+
+        public void OnClickClose()
+        {
+            this.gameObject.SetActive(false);
+        }
+
+    }
+}
---
+++ Assets/Script/Tutorial/TutorialManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject TutorialWindow;
+    public Pausable Pausable;
+
+
+
+    void Start()
+    {
+        OnClickTutorialButton();
+    }
+
+    public void OnClickTutorialButton()
+    {
+        // ゲームを一時停止
+        Pausable.pausing = true;
+
+        // チュートリアルウインドウを表示
+        TutorialWindow.SetActive(true);
+    }
+
+    public void OnClickTutorialWindowCloseButton()
+    {
+        // チュートリアルウインドウを閉じる
+        TutorialWindow.SetActive(false);
+
+        // ゲームを再開
+        Pausable.pausing = false;
+    }
+
+}
