665c1bd41451fddaf96f56b3e3f7c2caa990ca3f
IwazakiMakoto <nekozita1029@gmail.com>
2019-06-09 20:12:33 +0900

    ファイルパス管理ファイルの記述方法を変更、現状Audioファイルはファイル名で指定した方がいいので、それだけファイル名のみでアクセスできるようにした。 各Managerを経由し、タイトルでBGMとプレハブを生成し、Homeまで進めることを確認

---
+++ Assets/Nekozita/CommonTools/Resources/Scripts/Base/WindowBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowBase : MonoBehaviour
+{
+    protected WindowViewBase m_ViewBase;
+
+    /// <summary>
+    /// 指定したウインドウを開く
+    /// ※既にウインドウがあれば閉じて切り替える
+    /// </summary>
+    /// <param name="OpenWindowName">Open window.</param>
+    void OnOpenWindow(string OpenWindowName)
+    {
+        if (m_ViewBase.NowWindow != null) OnCloseWindow();
+
+        ObjectManager.Instance.OnPrefabLoad(OpenWindowName);
+
+        if (ObjectManager.Instance.OnPrefabLoad(OpenWindowName) != null && !m_ViewBase.NowWindow)
+        {
+            //NowWindow = ObjectManager.Instance.LastLoadObject;
+        }
+    }
+
+    /// <summary>
+    /// 指定したウインドウを閉じる
+    /// </summary>
+    void OnCloseWindow()
+    {
+        if (m_ViewBase.NowWindow == null) return;
+
+        Destroy(m_ViewBase.NowWindow);
+    }
+
+}
---
+++ Assets/Nekozita/CommonTools/Resources/Scripts/ObjectManager/ObjectManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObjectManager : SingletonMonoBehaviour<ObjectManager>
+{
+
+    // ロードしたプレハブ
+    [SerializeField] public GameObject LastLoadObject = null;
+
+    // ロードが完了したかどうかを管理する変数
+    private Action LastLoadExec = null;
+
+
+
+    private void Awake()
+    {
+        // シーン遷移後に削除されないようにする
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    /// <summary>
+    /// 指定したアセットをResoucesフォルダ内からロードする
+    /// プレハブのロードが成功しなければnullを返す
+    /// </summary>
+    /// <param name="LoadPrefabName">Load prefab.</param>
+    public Action OnPrefabLoad(string LoadPrefabName)
+    {
+        // プレハブをロードする
+        LastLoadObject = (GameObject)Resources.Load(LoadPrefabName);
+
+        // プレハブを元にインスタンスを生成する
+        Instantiate(LastLoadObject, LastLoadObject.transform.position, Quaternion.identity);
+
+        if (!LastLoadObject) return LastLoadExec;
+
+        return null;
+    }
+
+}
