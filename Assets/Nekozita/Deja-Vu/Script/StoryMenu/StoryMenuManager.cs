1cf601a1919f80494fb1a8d87098a7d785f0ca33
IwazakiMakoto <nekozita1029@gmail.com>
2019-07-01 00:30:42 +0900

    StoryNemuのUI基盤を構築

--- Assets/Nekozita/Deja-Vu/Script/StoryMenu/StoryMenuManager.cs
+++ Assets/Nekozita/Deja-Vu/Script/StoryMenu/StoryMenuManager.cs
@@ -2,20 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoryMenuManager : MonoBehaviour
+public class StoryMenuManager : GameManagerBase
 {
 
     void Start()
     {
-        // 使用していないアセットをアンロードしてメモリを解放
-        Resources.UnloadUnusedAssets();
-
         // シーンに必要なアセットをロード
-        ObjectManager.Instance.OnPrefabLoad(ResourcesPath.PREFAB_GRAPE);
         ObjectManager.Instance.OnPrefabLoad(ResourcesPath.PREFAB_WATER_FLOAR);
-
-        // シーンをフェードイン
-        SceneController.Instance.FadeIn();
 
         // BGMを再生(同じBGMが再生されていれば未処理)
         SoundManager.Instance.PlayBGM(ResourcesPath.AUDIO_BGM_HEALING_10);
