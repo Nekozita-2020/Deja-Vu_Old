d9030cafc26c0abb96c85fe632c3721bdd618a0d
IwazakiMakoto <nekozita1029@gmail.com>
2019-07-04 00:03:39 +0900

    セッティング画面のスライダー関連の処理実装

--- Assets/Nekozita/Deja-Vu/Script/Home/Setting/SettingWindow.cs
+++ Assets/Nekozita/Deja-Vu/Script/Home/Setting/SettingWindow.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingWindow : MonoBehaviour
+public class SettingWindow : WindowBase
 {
 
-    void Start()
-    {
-        
+	SettingWindowView View;
+
+	protected override void SettingView()
+	{
+		base.SettingView();
+
+		View = m_View as SettingWindowView;
+		View.m_ClickApplyButton = OnClickApplyButton;
+
+        View.MovementSensitivity_Value = PlayerPrefs.GetFloat("MovementSensitivity", 0.5F);
+        View.Slider_MovementSensitivity.value = View.MovementSensitivity_Value;
+    }
+
+	public void OnClickApplyButton()
+	{
+        // 操作感度を設定する
+        PlayerPrefs.SetFloat("MovementSensitivity", View.Slider_MovementSensitivity.value);
+        PlayerPrefs.Save();
     }
 
 }
--- Assets/Nekozita/Deja-Vu/Script/Home/Setting/SettingWindowView.cs
+++ Assets/Nekozita/Deja-Vu/Script/Home/Setting/SettingWindowView.cs
@@ -1,13 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SettingWindowView : MonoBehaviour
+public class SettingWindowView : WindowViewBase
 {
 
-    void Start()
+    // 感度設定関連
+    [SerializeField] public Slider Slider_MovementSensitivity = null;
+    public float MovementSensitivity_Value;
+
+    public Action m_ClickApplyButton = null;
+
+
+    public void OnClickApplyButton()
     {
-        
+        m_ClickApplyButton?.Invoke();
     }
 
 }
