914eb8a056315d69e446ec9874c33f6827c29f06
IwazakiMakoto <nekozita1029@gmail.com>
2019-07-03 23:17:21 +0900

    グレープの記憶を完成

--- Assets/Nekozita/Deja-Vu/Script/Home/GrapeMemory/GrapeMemoryWindow.cs
+++ Assets/Nekozita/Deja-Vu/Script/Home/GrapeMemory/GrapeMemoryWindow.cs
@@ -1,11 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GrapeMemoryWindow : MonoBehaviour
+public class GrapeMemoryWindow : WindowBase
 {
 
+    GrapeMemoryWindowView View;
 
+    protected override void SettingView()
+    {
+        base.SettingView();
+
+        View = m_View as GrapeMemoryWindowView;
+    }
+
+    void Start()
+    {
+        // ストーリーの進捗度を取得(クリアしたステージ数)
+        int StoryProgress = PlayerPrefs.GetInt("ClearStage", 20);
+
+        // 記憶が欠けているテキストを表示
+        View.StillLostMemoryText.gameObject.SetActive(true);
+        int ListNum = 0;
+
+        for (int i = 0; i <= StoryProgress; i++)
+        {
+            if (i == 3 || i == 5 || i == 8 || i == 10 ||
+                i == 13 || i == 15 || i == 18 || i == 19)
+            {
+                View.MemoryList[ListNum].gameObject.SetActive(true);
+                ListNum++;
+            }
+            else if(i == 20)
+            {
+                foreach (Text x in View.Memory_20) x.gameObject.SetActive(true);
+                View.StillLostMemoryText.gameObject.SetActive(false);
+            }
+        }
+    }
 
 }
--- Assets/Nekozita/Deja-Vu/Script/Home/GrapeMemory/GrapeMemoryWindowView.cs
+++ Assets/Nekozita/Deja-Vu/Script/Home/GrapeMemory/GrapeMemoryWindowView.cs
@@ -2,61 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class GrapeMemoryWindowView : MonoBehaviour
+public class GrapeMemoryWindowView : WindowViewBase
 {
 
-    [SerializeField] private Text StillLostMemoryText = null;
-    [SerializeField] private Text Memory_3 = null;
-    [SerializeField] private Text Memory_5 = null;
-    [SerializeField] private Text Memory_8 = null;
-    [SerializeField] private Text Memory_10 = null;
-    [SerializeField] private Text Memory_13 = null;
-    [SerializeField] private Text Memory_15 = null;
-    [SerializeField] private Text Memory_18 = null;
-    [SerializeField] private Text Memory_19 = null;
-    [SerializeField] private List<Text> Memory_20 = new List<Text>();
-
-    void Start()
-    {
-        // ストーリーの進捗度を取得(クリアしたステージ数)
-        int StoryProgress = PlayerPrefs.GetInt("ClearStage", 0);
-
-        for (int i = 3; i <= StoryProgress; i++)
-        {
-            switch (i)
-            {
-                case 3:
-                    Memory_3.gameObject.SetActive(true);
-                    break;
-                case 5:
-                    Memory_5.gameObject.SetActive(true);
-                    break;
-                case 8:
-                    Memory_8.gameObject.SetActive(true);
-                    break;
-                case 10:
-                    Memory_10.gameObject.SetActive(true);
-                    break;
-                case 13:
-                    Memory_13.gameObject.SetActive(true);
-                    break;
-                case 15:
-                    Memory_15.gameObject.SetActive(true);
-                    break;
-                case 18:
-                    Memory_18.gameObject.SetActive(true);
-                    break;
-                case 19:
-                    Memory_19.gameObject.SetActive(true);
-                    break;
-                case 20:
-                    foreach (Text x in Memory_20) x.gameObject.SetActive(true);
-                    StillLostMemoryText.gameObject.SetActive(false);
-                    break;
-            }
-        }
-    }
-
+    [SerializeField] public Text StillLostMemoryText = null;
+    [SerializeField] public List<Text> MemoryList = new List<Text>();
+    [SerializeField] public List<Text> Memory_20 = new List<Text>();
 
 }
