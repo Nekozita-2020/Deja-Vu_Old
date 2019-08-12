258370c3c2ed7fc0e9b7051d0c7337bdf99c5414
IwazakiMakoto <nekozita1029@gmail.com>
2019-08-12 16:15:22 +0900

    【StoryMenu】BGMを鳴らし忘れた

--- Assets/Nekozita/Deja-Vu/Script/StoryMenu/StoryMenuManager.cs
+++ Assets/Nekozita/Deja-Vu/Script/StoryMenu/StoryMenuManager.cs
@@ -25,7 +25,7 @@ public class StoryMenuManager : GameManagerBase
         ObjectManager.Instance.OnPrefabLoad(ResourcesPath.PREFAB_WATER_FLOAR);
 
         // BGMを再生(同じBGMが再生されていれば未処理)
-        // SoundManager.Instance.PlayBGM(ResourcesPath.AUDIO_BGM_HEALING_10);
+        SoundManager.Instance.PlayBGM(ResourcesPath.AUDIO_BGM_HEALING_10);
 
         // UIをストーリーの進捗度に合わせる
         for(int i = 0; i <= PlayerPrefs.GetInt("ClearStage", 0); i++)
