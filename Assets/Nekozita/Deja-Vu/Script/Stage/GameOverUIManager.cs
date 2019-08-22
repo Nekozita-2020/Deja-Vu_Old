f79b9af3e14a91a0529205f87e34418f54d10f88
IwazakiMakoto <nekozita1029@gmail.com>
2019-08-22 10:02:24 +0900

    【StageTemplate】シーンのリスタート処理を実装

--- Assets/Nekozita/Deja-Vu/Script/Stage/GameOverUIManager.cs
+++ Assets/Nekozita/Deja-Vu/Script/Stage/GameOverUIManager.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverUIManager : GameManagerBase
 {
+
+    public void OnSceneRestart()
+    {
+        OnSceneChange(SceneManager.GetActiveScene().name);
+    }
 
 }
