900b2dad0fdd71280862b33319c865de443f16fe
IwazakiMakoto <nekozita1029@gmail.com>
2019-11-04 19:10:32 +0900

    Rotationを固定するスクリプトを作成

---
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameObject/FixedRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedRotation : MonoBehaviour
+{
+    [Header("固定する軸")]
+    [SerializeField] private bool Axis_X = false;
+    [SerializeField] private bool Axis_Y = false;
+    [SerializeField] private bool Axis_Z = false;
+
+    private void Update()
+    {
+        if (Axis_X) this.transform.rotation = Quaternion.Euler(0,
+            this.transform.rotation.y, this.transform.rotation.z);
+        if (Axis_Y) this.transform.rotation = Quaternion.Euler(this.transform.rotation.x,
+            0, this.transform.rotation.z);
+        if (Axis_Z) this.transform.rotation = Quaternion.Euler(this.transform.rotation.x,
+            this.transform.rotation.y, 0);
+    }
+}
