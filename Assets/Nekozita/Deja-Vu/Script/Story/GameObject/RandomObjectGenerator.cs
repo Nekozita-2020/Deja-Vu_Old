f1fd21ef3790bbc8e59bee14112cbd94fb4ca6a1
IwazakiMakoto <nekozita1029@gmail.com>
2019-11-13 03:02:23 +0900

    【ステージ1】ステージに配置するオブジェクトを実装

---
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameObject/RandomObjectGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectGenerator : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> ObjectList = null;
+
+    private void Start()
+    {
+		OnRandomObjectGenerate();
+	}
+
+    private void OnRandomObjectGenerate()
+	{
+        int ListNum = Random.Range(0, ObjectList.Count);
+        Instantiate(ObjectList[ListNum], this.transform);
+	}
+
+}
---
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameObject/RandomSetPosition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSetPosition : MonoBehaviour
+{
+	[Header("ランダム配置する軸")]
+	[SerializeField] private bool Axis_X = false;
+	[SerializeField] private bool Axis_Y = false;
+	[SerializeField] private bool Axis_Z = false;
+
+    [Header("ランダム配置する幅")]
+    [SerializeField] private float Width = 0.0f;
+
+    private void Start()
+    {
+        OnRandomSetPosition();
+    }
+
+    private void OnRandomSetPosition()
+    {
+        float RandomWidth = Random.Range(-Width, Width);
+
+        if (Axis_X)
+        {
+            this.transform.localPosition = new Vector3(RandomWidth,
+                this.transform.localPosition.y, this.transform.localPosition.z);
+        }
+        if (Axis_Y)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x,
+                RandomWidth, this.transform.localPosition.z);
+        }
+        if (Axis_Z)
+        {
+            this.transform.localPosition = new Vector3(this.transform.localPosition.x,
+                this.transform.localPosition.y, RandomWidth);
+        }
+    }
+}
