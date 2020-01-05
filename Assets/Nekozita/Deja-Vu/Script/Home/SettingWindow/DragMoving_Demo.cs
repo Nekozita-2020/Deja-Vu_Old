ae1a183821555c9924bcedfc89051622063e4ed0
IwazakiMakoto <nekozita1029@gmail.com>
2020-01-05 15:01:46 +0900

    【SettingWindow&StageSelect】 ・セッティング画面で、グレープが水の下まで移動できてしまう不具合を修正 ・ステージ4の説明文の誤字を修正

---
+++ Assets/Nekozita/Deja-Vu/Script/Home/SettingWindow/DragMoving_Demo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// セッティングウインドウでは移動制限を設定したいから継承させて使ってる
+/// </summary>
+public class DragMoving_Demo : DragMoving
+{
+
+    public void OnDrag_Demo()
+    {
+        var x = Input.GetAxis("Mouse X");
+        var y = Input.GetAxis("Mouse Y");
+
+        transform.position = new Vector3
+            (Mathf.Clamp(transform.position.x +
+            (x * MovementSensitivity), DataStore.MovementRange.LimitLeft, DataStore.MovementRange.LimitRight),
+            Mathf.Clamp(transform.position.y +
+            (y * MovementSensitivity), DataStore.MovementRange.LimitBottom + 4, DataStore.MovementRange.LimitTop),
+            transform.position.z);
+    }
+
+}
