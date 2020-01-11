cff69f8ac410d84a90e1d257284639f7e18a90ea
IwazakiMakoto <nekozita1029@gmail.com>
2020-01-11 17:05:09 +0900

    【ステージ2】 ・取得されたエフェクトを作成 ・アイテムボックスの取得時の処理を実装

--- Assets/Nekozita/Deja-Vu/Script/Story/GameObject/ItemBox.cs
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameObject/ItemBox.cs
@@ -11,12 +11,37 @@ public class ItemBox : MonoBehaviour
 
     // このアイテムを取得した時のエフェクト
     [SerializeField] ParticleSystem m_GetItemEffect = null;
- 
+
+
+
     private void OnTriggerEnter(Collider m_Collision)
     {
         this.m_CallBack?.Invoke(m_Collision);
 
-        // ボックスを消して取得エフェクトを出す
+        if(m_Collision.gameObject.tag == "Grape")
+        {
+            // このアイテムが取得された時の処理
+            OnAcquiredItem();
+        }
     }
 
+    /// <summary>
+    /// このアイテムが取得された時の処理
+    /// </summary>
+    private void OnAcquiredItem()
+    {
+        OnEffectPlay(this.m_GetItemEffect);
+        SoundManager.Instance.PlaySE(ResourcesPath.AUDIO_SE_DESTROY_SOUND);
+        OnDestroy();
+    }
+
+    private void OnEffectPlay(ParticleSystem _TargetEffect)
+    {
+        Instantiate(_TargetEffect, this.transform.position, Quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(this.gameObject);
+    }
 }
