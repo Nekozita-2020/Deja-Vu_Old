1aaf868b22647e75fdfa0b1816a3aeb23f050be4
IwazakiMakoto <nekozita1029@gmail.com>
2020-01-12 14:47:32 +0900

    【ステージ2】 ・不要な画像を削除 ・GameManagerに諸々アタッチした

--- Assets/Nekozita/Deja-Vu/Script/Story/GameManager/Stage2_GameManager.cs
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameManager/Stage2_GameManager.cs
@@ -8,11 +8,14 @@ public class Stage2_GameManager : StageGameManagerBase
     // アイテムの画像リスト
     [SerializeField] private List<Sprite> m_ItemImageList = null;
 
-    // 所持アイテムを表示する
+    // 所持アイテムを表示するUI
     [SerializeField] private List<Image> m_Belongings = null;
 
-    // ステージに配置されてるアイテムボックスリスト
-    [SerializeField] private List<ItemBox> m_ItemBoxList = null;
+    // ステージに配置されてるアイテムボックスリスト1〜4
+    [SerializeField] private List<ItemBox> m_ItemBoxList1 = null;
+    [SerializeField] private List<ItemBox> m_ItemBoxList2 = null;
+    [SerializeField] private List<ItemBox> m_ItemBoxList3 = null;
+    [SerializeField] private List<ItemBox> m_ItemBoxList4 = null;
 
 
 
