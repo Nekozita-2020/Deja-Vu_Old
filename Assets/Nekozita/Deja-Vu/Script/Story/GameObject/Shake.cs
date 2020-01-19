37a220897024d44b3046f7b3ecd28a63ced7e5c1
IwazakiMakoto <nekozita1029@gmail.com>
2020-01-20 02:39:45 +0900

    【ステージ2】 ・花と蜂のプレハブを作成 ・最低限のクオリティでステージ2の処理完了

--- Assets/Nekozita/Deja-Vu/Script/Story/GameObject/Shake.cs
+++ Assets/Nekozita/Deja-Vu/Script/Story/GameObject/Shake.cs
@@ -8,24 +8,43 @@ public class Shake : MonoBehaviour
     [SerializeField] private bool Horizontal = false;
     [SerializeField] private bool Vertical = false;
 
+    [Header("回転方向")]
+    // true: 時計回り false: 反時計回り
+    [SerializeField] private bool ClockWise = true;
+
     [Header("移動範囲")]
     [SerializeField] private float Width = 1.0f;
     [SerializeField] private float Height = 1.0f;
 
     [Header("スピード")]
+    [SerializeField] private bool RamdomSpeed = false;
+    [SerializeField] private float RamdomLow = 1.0f;
+    [SerializeField] private float RamdomUpper = 3.0f;
     [SerializeField] private float speed = 1.0f;
 
-    // 値を保存する用
-    private float Elapsed = 0.0f;
+    // 移動先座標の値を保存する用
+    private float Elapsed = 0.5f;
 
 
+
+    private void Start()
+    {
+        // ランダムフラグがOnならspeedを更新
+        if (RamdomSpeed)
+        {
+            speed = Random.Range(RamdomLow, RamdomUpper);
+        }
+    }
 
     private void FixedUpdate()
     {
         // いずれかの移動方向が指定されている時
         if(Horizontal || Vertical)
         {
-            Elapsed += Time.deltaTime * speed;
+            if(ClockWise)
+                Elapsed -= Time.deltaTime * speed;
+            else
+                Elapsed += Time.deltaTime * speed;
 
             // 水平方向
             if (Horizontal)
