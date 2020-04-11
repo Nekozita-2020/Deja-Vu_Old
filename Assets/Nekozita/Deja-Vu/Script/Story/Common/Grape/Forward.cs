01f357eaaac6e3d9a92160d3616e901635a24a04
IwazakiMakoto <nekozita1029@gmail.com>
2020-04-11 10:26:00 +0900

    大量のmetaデータに謎の差分が出たので更新

--- Assets/Nekozita/Deja-Vu/Script/Story/Common/Grape/Forward.cs
+++ Assets/Nekozita/Deja-Vu/Script/Story/Common/Grape/Forward.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Forward : MonoBehaviour
 {
-
     [Tooltip("進行速度")]
     public float ForwardSpeed = 20.0f;
 
-    [Tooltip("進行方向　true: 奥へ false: 手前へ")]
+    [Tooltip("進行速度(一定)をランダムにするか")]
+    [SerializeField] private bool IsRamdom_InitialSpeed = false;
+
+    [Tooltip("ランダム速度範囲")]
+    public float m_RamdomSpeedLower = 0.0f;
+    public float m_RamdomSpeedUpper = 10.0f;
+
+    [Tooltip("進行方向　true: 向いてる方向へ false: 逆向きへ")]
     [SerializeField] private bool IsForward = true;
-        
+
+
+
+    private void Start()
+    {
+        if (IsRamdom_InitialSpeed)
+        {
+            // ランダム速度が指定されてる場合は速度をランダムにセット
+            ForwardSpeed = Random.Range(m_RamdomSpeedLower, m_RamdomSpeedUpper);
+        }
+    }
+
     void FixedUpdate()
     {
         if(IsForward == true)
---
+++ Assets/Nekozita/Deja-Vu/Script/Story/Stage4/SlotMachine.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotMachine : MonoBehaviour
+{
+    // 生成するオブジェクト郡
+    [SerializeField] private List<GameObject> m_CoinsObjList = null;
+
+    [Header("オブジェクトの生成間隔秒(〜秒毎に生成)")]
+    [SerializeField] private float GenerateInterval = 1.0f;
+
+    // オブジェクト生成をするタイミングかどうかのフラグ
+    private bool m_GanarateFlug = false;
+
+    // 時間管理用
+    private float m_Time = 0.0f;
+
+
+
+    private void Update()
+    {
+        if (m_GanarateFlug == true)
+        {
+            m_Time += Time.deltaTime;
+
+            if (GenerateInterval < m_Time)
+            {
+                // 指定秒毎に生成
+                GenerateToObject();
+
+                m_Time = 0.0f;
+            }
+        }
+    }
+
+    /// <summary>
+    /// オブジェクト群からオブジェクトを生成
+    /// </summary>
+    private void GenerateToObject()
+    {
+        int GenerateNum = Random.Range(0, m_CoinsObjList.Count);
+        var Obj = Instantiate(m_CoinsObjList[GenerateNum], this.gameObject.transform);
+
+        // オブジェクトの向きをランダムにする
+        float m_RandomPos_x = Random.Range(DataStore.MovementRange.LimitLeft, DataStore.MovementRange.LimitRight);
+        Vector3 RandomAngle = new Vector3(m_RandomPos_x, 0, 0);
+        Obj.transform.LookAt(RandomAngle);
+
+        // 生成したオブジェクトに消滅する条件を与えておく
+        Destroy(Obj, 3.0f);
+    }
+}
