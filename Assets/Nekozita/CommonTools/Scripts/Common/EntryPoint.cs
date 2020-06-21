4f3fa11638aa55e09b2332e6adaf1c383ac8b5e9
ネコジタ <>
2020-06-21 19:27:00 +0900

    【リポジトリ内の整理】 ・Standard Assets削除、その他不要なもの削除 ・UniRxおよびUniTaskの導入 ・EntryPoint.cs(起動時処理)を一部リファクタリング

---
+++ Assets/Nekozita/CommonTools/Scripts/Common/EntryPoint.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// ゲーム起動時に実行させる処理をまとめたクラス
+/// </summary>
+public class EntryPoint : SingletonMonoBehaviour<EntryPoint>
+{
+    [RuntimeInitializeOnLoadMethod()]
+    static async void Init()
+    {
+        // フレームレートを30FPSに設定
+        Application.targetFrameRate = 30;
+
+        // 追加予定のCommonToolsが有効でない時(まだ読み込んでいない時)だけ追加ロードするように
+        if (!SceneManager.GetSceneByName("CommonTools").IsValid())
+        {
+            await SceneManager.LoadSceneAsync("CommonTools", LoadSceneMode.Additive);
+
+            /* 進捗とか表示する用の「UniTask Ver2」用の書き方
+            await SceneManager.LoadSceneAsync("CommonTools", LoadSceneMode.Additive)
+                .ToUniTask(
+                Progress.Create<float>(n => Debug.Log($"現在{n * 100}%"))
+                );
+            */
+        }
+    }
+
+    /*
+    private IEnumerator Start()
+    {
+        // フレームレートを30FPSに設定
+        Application.targetFrameRate = 30;
+
+        // 追加予定のCommonToolsが有効でない時(まだ読み込んでいない時)だけ追加ロードするように
+        if (!SceneManager.GetSceneByName("CommonTools").IsValid())
+        {
+            // イベントにイベントハンドラーを追加
+            SceneManager.sceneLoaded += OnMakeCommonTools;
+            SceneManager.LoadScene("CommonTools", LoadSceneMode.Additive);
+        }
+
+        // ロゴウインドウのフェードが完了するまで待つ
+        yield return new WaitUntil(() => m_LogoWindowView.FadeCompleteFlag);
+
+        // CommonToolsが生成されるまで待つ
+        yield return new WaitUntil(() => MakeToolsFlag);
+
+        // タイトルスタート
+        m_TitleManager.gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// CommonToolsが生成された時の処理
+    /// </summary>
+    /// <param name="m_LoadScene"></param>
+    /// <param name="m_mode"></param>
+    private void OnMakeCommonTools(Scene m_LoadScene, LoadSceneMode m_mode)
+    {
+        MakeToolsFlag = true;
+    }
+    */
+
+}
