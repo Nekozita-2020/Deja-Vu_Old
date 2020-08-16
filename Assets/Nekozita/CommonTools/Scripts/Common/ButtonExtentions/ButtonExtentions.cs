82d3b167ed5bbefda5712fe800f03fdf37f712af
ネコジタ <>
2020-08-17 01:24:22 +0900

    【ButtonExtentions】 ・ボタン拡張スクリプトを実装していく

---
+++ Assets/Nekozita/CommonTools/Scripts/Common/ButtonExtentions/ButtonExtentions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonExtentions : Selectable
+{
+	public override void OnPointerDown(PointerEventData eventData) { }
+
+	public override void OnPointerUp(PointerEventData eventData) { }
+
+	public override void OnPointerEnter(PointerEventData eventData) { }
+
+	public override void OnPointerExit(PointerEventData eventData) { }
+}
